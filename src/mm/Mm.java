97200f23a2267f213c9bbdc721ba0cb9feaf8a7c
EkatUla2022 <reikon@list.ru>
2022-05-14 16:53:43 +0300

    File file  will be checked  rename MM

--- src/mm/Mm.java
+++ src/mm/Mm.java
@@ -1,6 +1,6 @@
 package mm;
 
-public class MM {
+public class Mm {
     public static void main(String[] args){
         System.out.println("Hello from Sales");
         Byte bt1 = 127;
@@ -9,7 +9,11 @@ public class MM {
         Long l1 = 13L;
         Float fl1 = 14.5F;
         Double db = 16.6;
-        System.out.println(Double.MAX_VALUE);
-        System.out.println(Double.MIN_VALUE);
+        Character cr = Character.MIN_VALUE;
+        System.out.println((int) cr);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
+
     }
 }
+
