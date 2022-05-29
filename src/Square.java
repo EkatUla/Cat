3f7b3e0548776f398da3a3c16f9a6be84da0ab70
EkatUla2022 <reikon@list.ru>
2022-05-30 01:06:29 +0300

    new task

---
+++ src/Square.java
@@ -0,0 +1,11 @@
+public class Square {
+    public static void main(String[] args) {
+        int S;
+        S = area(120);
+        System.out.println("Square area = " + S);
+    }
+
+    public static int area(int a){
+        return a * a;
+    }
+}
